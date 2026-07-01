99a8ce90c63fe4a2129a3793b67914bd444c7c15
Enrique P <enrique.perez.dev.mx@gmail.com>
2026-07-01 12:22:41 -0600

    Arreglo de ruta del cv importando el archivo en vite

--- src/data/dictionary.js
+++ src/data/dictionary.js
@@ -1,12 +1,13 @@
 import { es } from './es';
 import { en } from './en';
+import cvFile from './cv-enrique.pdf'; // <-- 1. Importas el PDF aquí arriba
 
 export const commonData = {
   email: "enrique.perez.dev.mx@gmail.com",
   whatsapp: "https://wa.me/525528529994",
   github: "https://github.com/enriqueperezdevmx-coder",
   linkedin: "https://linkedin.com/in/enrique-perez-sanchez-dev-cdmx",
-  cvLink: import.meta.env.BASE_URL + "cv-enrique.pdf",
+  cvLink: cvFile, // <-- 2. Asignas la variable aquí, sin comillas
   skills: {
     frontend: ["React", "JavaScript (ES6+)", "HTML5", "CSS3", "Tailwind CSS"],
     backend: ["Java", "Spring Boot", "Node.js", "SQL"],
